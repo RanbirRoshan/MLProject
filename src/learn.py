cd1981c34fd0f366f2ad2fb1f3e9aff9e05db014
Ranbir Roshan <ranbirroshan@live.in>
2019-03-26 10:32:50 -0400

    Add main function in program
    Signed-off-by: Ranbir Roshan <ranbirroshan@live.in>

--- src/learn.py
+++ src/learn.py
@@ -3,13 +3,18 @@ from src.utility import dataUtility
 from src.baseline_implementation import baseline
 
 
-# define constants here
-data_file_name = 'music_csv_data\data_prof.csv' #'music_csv_data\data.csv'#
+def main():
+    # define constants here
+    data_file_name = 'music_csv_data\data_prof.csv' #'music_csv_data\data.csv'#
 
-# musicUtility.generate_music_data("TrainingData", data_file_name)
+    # musicUtility.generate_music_data("TrainingData", data_file_name)
 
-# load the data in memory
-X_train, X_test, y_train, y_test, is_multi_class = dataUtility.get_test_train_set(data_file_name, 0.8, False)
+    # load the data in memory
+    X_train, X_test, y_train, y_test, is_multi_class = dataUtility.get_test_train_set(data_file_name, 0.8, False)
 
-# baseline implementations
-baseline.run_all_baselines(X_train, X_test, y_train, y_test, is_multi_class)
+    # baseline implementations
+    baseline.run_all_baselines(X_train, X_test, y_train, y_test, is_multi_class)
+
+
+if __name__ == '__main__':
+    main()
